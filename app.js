b160bd33b44f3554c500ed7481ec4175ef53b33d
yash-surana <75572841+yash-surana@users.noreply.github.com>
2021-03-26 23:44:22 +0530

    input output processing

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var btnTranslate = document.querySelector('#btn-translate');
 
 // variable txtInput
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output")
+
 
 // button to translate gives console output clicked
 btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("clicked")
-    console.log("input: ",txtInput.value);
+    outputDiv.innerText = "translated from whatever I knew: " + txtInput.value;
 })
 
